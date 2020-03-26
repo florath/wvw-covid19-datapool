1e3c691baee215d2efb4208516f8823e0cfb87e2
teraadmin <teraadmin@teraadmins-MacBook-Pro-3.local>
2020-03-26 12:53:05 +0100

    add TODOs

---
+++ dbsync/data_import/stadt_muenster.py
@@ -0,0 +1,10 @@
+'''
+File which implements the data import of
+the Stadt Muenster data set.
+'''
+
+# TODO: implement function, which reads a csv file
+# TODO: implement function, which handles one data line
+# TODO: implement function, which updates data
+# TODO: implement function, which imports one data set
+# TODO: implement function, which opens the right repo
