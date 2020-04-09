eeb57c96a470c32b54ca8ac1d7ed9da12a894031
Andreas Florath <andreas@florath.net>
2020-04-09 16:33:44 +0200

    Add parse args file
    Signed-off-by: Andreas Florath <andreas@florath.net>

---
+++ dbsync/covid19datapool_dbsync/lib/parse_args.py
@@ -0,0 +1,14 @@
+'''
+Parse command line arguments
+'''
+
+import argparse
+
+
+def parse_args_common():
+    parser = argparse.ArgumentParser(description='common args.')
+    parser.add_argument('--dbenv', type=str, required=True,
+                        help='Name of the database backend; '
+                        'options: google_firestore, python_json')
+    args = parser.parse_args()
+    return args.dbenv
