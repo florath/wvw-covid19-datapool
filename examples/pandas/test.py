cb7b9c426e9e468a9f560533b60f110eefaa4985
crisstels <natalie-hasselmann@web.de>
2020-04-22 13:17:39 +0200

    plots data from Germany

--- examples/pandas/test.py
+++ examples/pandas/test.py
@@ -1,27 +1,46 @@
+import datetime
+import json
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def convert_timestamp(dataframe):
+    dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='s')
+
+
+def insert_date(dataframe):
+    '''Inserts a date in year/month/date format'''
+    dataframe.insert(0, "date",
+                     dataframe.timestamp.apply(
+                         lambda x: datetime.datetime(day=x.date().day,
+                                                     month=x.date().month,
+                                                     year=x.date().year)))
+
+
 def filter_columns(dataframe):
-    country = 'US'
-    return dataframe.loc[(dataframe['Country_Region'] == country)]
+    '''Filters data by country and dates. Here: Germany in April 2020'''
+    country = 'DE'
+    return dataframe.loc[(dataframe['iso-3166-1'] == country) & (dataframe['date'] > datetime.datetime(2020, 4, 1))]
 
 
-def delete_columns(data):
-    data = filter_columns(data)
-    data.drop(labels=['FIPS', 'Admin2', 'Lat', 'Long_', 'Active', 'Last_Update', 'Country_Region', 'Combined_Key',
-                      'Province_State', 'Confirmed'], axis=1, inplace=True)
-
-    return data
+def get_total_deaths(dataframe):
+    dataframe.drop(labels=['timestamp', 'iso-3166-1'], axis=1, inplace=True)
+    sum_deaths = dataframe.sum(axis=0, skipna=True)
+    return sum_deaths
 
 
-URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-14-2020.csv"
-
+path = "/Users/teraadmin/Desktop/John Hopkins/jh.json"
 plt.close('all')
-# read csv files from April
-csv = pd.read_csv(URL, error_bad_lines=False)
-csv = filter_columns(csv)
-print(csv.head(10))
+# read json files from John Hopkins University
+with open(path) as json_file:
+    data = json.load(json_file)
+    useful_data = data[1]
 
-# csv.plot.bar()
-# plt.show()
+df = pd.DataFrame(useful_data)
+df.drop(labels=['source', 'original', 'latitude', 'longitude'], axis=1, inplace=True)
+convert_timestamp(df)
+insert_date(df)
+df = filter_columns(df)
+ax = df.plot(x='date', y=['infected_total', 'recovered_total'])
+plt.show()
